952ece97c3ab3151edc82ff55cbef07322b0f422
Tobias K <55653566+tobiaskircher@users.noreply.github.com>
2021-04-07 13:34:02 +0100

    button functionality
    added one button. it draws a rectangle with centered text. it changes colour when being hovered above. when pressed it currently calls a test function

--- Project/cube_solver.py
+++ Project/cube_solver.py
@@ -2,6 +2,8 @@ import pygame
 
 BLACK = (0,0,0)
 WHITE = (255,255,255)
+GREY = (107,107,107)
+LIGHT_GREY = (140,140,140)
 
 #Initialise PyGame
 pygame.init()
@@ -19,6 +21,24 @@ done = False
 #Manages how quick screen refreshes
 clock = pygame.time.Clock()
 
+def test_function():
+    print("Test Function Called.")
+    
+def button(text, x, y, width, height, inactive, active, func=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    if x+width > mouse[0] > x and  y+height > mouse[1] > y:
+        pygame.draw.rect(screen, active, (x,y,width,height))
+        if click[0] == 1 and func != None:
+            func()
+    else:
+        pygame.draw.rect(screen, inactive, (x,y,width,height))
+
+    font = pygame.font.Font("freesansbold.ttf",20)
+    text_surface, text_rect = font.render(text,True,BLACK), font.render(text,True,BLACK).get_rect()
+    text_rect.center = (x+(width//2),y+(height//2))
+    screen.blit(text_surface, text_rect)
+
 #Game Loop
 while not done:
     
@@ -28,7 +48,10 @@ while not done:
             done = True
             
     #Blank black screen
-    screen.fill (BLACK)
+    screen.fill(BLACK)
+
+    #Logic
+    button("Solve",220,150,200,50,GREY,LIGHT_GREY, test_function)
 
     #Flip display
     pygame.display.flip()

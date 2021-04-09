e40372e00ebf6900f148fd4e99a46eeab7fceaad
Tobias K <55653566+tobiaskircher@users.noreply.github.com>
2021-04-09 11:38:24 +0100

    basic screens
    added basic screens for all sections. also rearranged the code and added classes

--- Project/cube_solver.py
+++ Project/cube_solver.py
@@ -22,43 +22,82 @@ done = False
 #Manages how quick screen refreshes
 clock = pygame.time.Clock()
 
-def get_face():
-    face = scan_face.run()
-    print(face[0:3])
-    print(face[3:6])
-    print(face[6:9])
+class UI():
+    def text(content, size, x, y, colour=BLACK):
+        font = pygame.font.Font("freesansbold.ttf", size)
+        text_surface = font.render(content,True,colour)
+        text_rect = font.render(content,True,colour).get_rect()
+        text_rect.center = (x,y)
+        screen.blit(text_surface, text_rect)
+        
+    def button(msg, x, y, width, height, inactive, active, func=None):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        if x+width > mouse[0] > x and  y+height > mouse[1] > y:
+            pygame.draw.rect(screen, active, (x,y,width,height))
+            if click[0] == 1 and func != None:
+                func()
+        else:
+            pygame.draw.rect(screen, inactive, (x,y,width,height))
 
-def timer():
-    print("Timer.")
+        UI.text(msg, 20, x+(width//2), y+(height//2))
 
-def learn():
-    print("Learn.")
+class ButtonFunctions():
+    def get_face():
+        face = scan_face.run()
+        print(face[0:3])
+        print(face[3:6])
+        print(face[6:9])
 
-def help():
-    print("Help.")
-    
-def test_function():
-    print("Test Function Called.")
+    def timer():
+        game_state.state = "timer"
 
-def text(content, size, x, y, colour=BLACK):
-    font = pygame.font.Font("freesansbold.ttf", size)
-    text_surface = font.render(content,True,colour)
-    text_rect = font.render(content,True,colour).get_rect()
-    text_rect.center = (x,y)
-    screen.blit(text_surface, text_rect)
-    
-def button(msg, x, y, width, height, inactive, active, func=None):
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    if x+width > mouse[0] > x and  y+height > mouse[1] > y:
-        pygame.draw.rect(screen, active, (x,y,width,height))
-        if click[0] == 1 and func != None:
-            func()
-    else:
-        pygame.draw.rect(screen, inactive, (x,y,width,height))
+    def learn():
+        game_state.state = "learn"
 
-    text(msg, 20, x+(width//2), y+(height//2))
+    def help():
+        game_state.state = "help"
 
+class GameState():
+    def __init__(self):
+        self.state = "menu"
+
+    def state_manager(self):
+        if self.state == "menu":
+            self.menu_screen()
+        elif self.state == "timer":
+            self.timer_screen()
+        elif self.state == "learn":
+            self.learn_screen()
+        elif self.state == "help":
+            self.help_screen()
+
+    def menu_screen(self):
+        UI.button("Solve",220,150,200,50,GREY,LIGHT_GREY, ButtonFunctions.get_face)
+        UI.button("Timer",220,210,200,50,GREY,LIGHT_GREY, ButtonFunctions.timer)
+        UI.button("Learn",220,270,200,50,GREY,LIGHT_GREY, ButtonFunctions.learn)
+        UI.button("Help",220,330,200,50,GREY,LIGHT_GREY, ButtonFunctions.help)
+        UI.text("CUBE SOLVER", 50, 320, 80,WHITE)
+
+    def return_to_menu(self):
+        self.state = "menu"
+        
+    def timer_screen(self):
+        UI.button("Back To Menu",10,10,150,30,GREY,LIGHT_GREY, self.return_to_menu)
+        UI.text("TIMER", 50, 320, 30,WHITE)
+
+    def learn_screen(self):
+        UI.button("Back To Menu",10,10,150,30,GREY,LIGHT_GREY, self.return_to_menu)
+        UI.text("LEARN", 50, 320, 30,WHITE)
+
+    def help_screen(self):
+        UI.button("Back To Menu",10,10,150,30,GREY,LIGHT_GREY, self.return_to_menu)
+        UI.text("HELP", 50, 320, 30,WHITE)
+        
+        
+
+
+game_state = GameState()
 
 #Game Loop
 while not done:
@@ -72,12 +111,8 @@ while not done:
     screen.fill(BLACK)
 
     #Logic
-    button("Solve",220,150,200,50,GREY,LIGHT_GREY, get_face)
-    button("Timer",220,210,200,50,GREY,LIGHT_GREY, timer)
-    button("Learn",220,270,200,50,GREY,LIGHT_GREY, learn)
-    button("Help",220,330,200,50,GREY,LIGHT_GREY, help)
-    text("CUBE SOLVER", 50, 320, 80,WHITE)
-
+    game_state.state_manager()
+    
     #Flip display
     pygame.display.flip()
     

53cd80ef522de00cf5fbb6ea646cc6fdb7df291a
Tobias K <55653566+tobiaskircher@users.noreply.github.com>
2021-04-18 20:28:00 +0100

    colour picker
    added colour picker to allow to you to make corrections. it only lets you confirm once every square on the grid is assigned a colour

--- Project/cube_solver.py
+++ Project/cube_solver.py
@@ -4,10 +4,35 @@ import time
 import random
 import solution_generator
 
+#COLOURS
 BLACK = (0,0,0)
-WHITE = (255,255,255)
 GREY = (107,107,107)
 LIGHT_GREY = (140,140,140)
+WHITE = (255,255,255)
+GREEN = (0,255,0)
+RED = (255,0,0)
+ORANGE = (255,165,0)
+BLUE = (0,0,255)
+YELLOW = (255,255,0)
+
+colours_dic = {
+    "w":WHITE,
+    "g":GREEN,
+    "r":RED,
+    "o":ORANGE,
+    "b":BLUE,
+    "y":YELLOW
+    }
+
+colours_name = {
+    "w":"White",
+    "g":"Green",
+    "r":"Red",
+    "o":"Orange",
+    "b":"Blue",
+    "y":"Yellow"
+    }
+
 
 #Initialise PyGame
 pygame.init()
@@ -33,13 +58,16 @@ class UI():
         text_rect.center = (x,y)
         screen.blit(text_surface, text_rect)
         
-    def button(msg, x, y, width, height, inactive, active, func=None):
+    def button(msg, x, y, width, height, inactive, active, func=None, *params):
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
         if x+width > mouse[0] > x and  y+height > mouse[1] > y:
             pygame.draw.rect(screen, active, (x,y,width,height))
             if click[0] == 1 and func != None:
-                func()
+                if list(params) != []:
+                    func(params)
+                else:
+                    func()
         else:
             pygame.draw.rect(screen, inactive, (x,y,width,height))
 
@@ -49,6 +77,19 @@ class ButtonFunctions():
     def solve():
         game_state.state = "solve"
         game_state.faces_scanned = 0
+
+    def confirm_adjustments():
+        game_state.state = "solve"
+
+    def change_colour(params):
+        position = params[0]
+        game_state.face = list(game_state.face)
+        game_state.face[position] = game_state.equipped_colour
+        "".join(game_state.face)
+
+    def equip_colour(params):
+        colour = params[0]
+        game_state.equipped_colour = colour
 
     def timer():
         game_state.state = "timer"
@@ -94,12 +135,19 @@ class GameState():
             }
 
         self.cube = ""
+        self.equipped_colour = "w"
 
     def state_manager(self):
         if self.state == "menu":
             self.menu_screen()
+            
         elif self.state == "solve":
             self.solve_screen()
+        elif self.state == "solve_correction_screen":
+            self.solve_correction_screen()
+        elif self.state == "solve_solution_screen":
+            self.solve_solution_screen()
+            
         elif self.state == "timer":
             self.timer_screen()
         elif self.state == "learn":
@@ -123,16 +171,54 @@ class GameState():
         pygame.display.flip()
         
 
-        face = scan_face.run(self.faces_to_scan["facing_camera"][self.faces_scanned],self.faces_to_scan["facing_up"][self.faces_scanned])
+        self.face = scan_face.run(self.faces_to_scan["facing_camera"][self.faces_scanned],self.faces_to_scan["facing_up"][self.faces_scanned])
         self.faces_scanned += 1
-        '''print("\n")
-        print(self.faces_to_scan["facing_camera"][count])
+
+        if self.faces_scanned < 7:
+            self.state = "solve_correction_screen"
+        else:
+            self.state = "solve_solution_screen"
+            
+        '''
         print(face[0:3])
         print(face[3:6])
-        print(face[6:9])
-        count += 1'''
+        print(face[6:9])'''
 
-    def correction_screen(self):
+    def solve_correction_screen(self):
+        UI.text("ADJUSTMENTS", 50, 320, 30,WHITE)
+        UI.text("Please Make Any Adjustments Required Here.", 25, 320, 70,WHITE)
+        UI.text("Make Sure All Parts Are Filled With The Correct Colour.", 20, 320, 100,WHITE)
+        grid_width = 40
+        grid_center = [160,240]
+        grid_gap = 10
+
+        grid_rows = [grid_center[1] - grid_gap - 1.5*grid_width ,grid_center[1] - grid_width//2, grid_center[1] + grid_width//2 + grid_gap]
+        grid_columns = [grid_center[0] - grid_gap - 1.5*grid_width ,grid_center[0] - grid_width//2, grid_center[0] + grid_width//2 + grid_gap]
+
+        count = 0
+        for y in grid_rows:
+            for x in grid_columns:
+                if self.face[count] != "?":
+                    UI.button("",x,y,40,40,colours_dic[self.face[count]],colours_dic[self.face[count]], ButtonFunctions.change_colour, count)
+                else:
+                    UI.button("?",x,y,40,40,GREY,GREY, ButtonFunctions.change_colour, count)
+                count += 1
+
+        UI.button("",410,220,40,40,WHITE,WHITE, ButtonFunctions.equip_colour, "w")
+        UI.button("",460,220,40,40,GREEN,GREEN, ButtonFunctions.equip_colour, "g")
+        UI.button("",510,220,40,40,BLUE,BLUE, ButtonFunctions.equip_colour, "b")
+        UI.button("",410,270,40,40,RED,RED, ButtonFunctions.equip_colour, "r")
+        UI.button("",460,270,40,40,ORANGE,ORANGE, ButtonFunctions.equip_colour, "o")
+        UI.button("",510,270,40,40,YELLOW,YELLOW, ButtonFunctions.equip_colour, "y")
+        
+        UI.text("Colour Picker:", 25, 480, 190,WHITE)
+        equipped_colour_text = "Equipped Colour: " + colours_name[self.equipped_colour]
+        UI.text(equipped_colour_text, 20, 480, 340,WHITE)
+        if "?" not in self.face:
+            UI.button("Confirm",280,430,80,40,GREY,LIGHT_GREY, ButtonFunctions.confirm_adjustments)
+        
+
+    def solve_solution_screen(self):
         pass
         
     def timer_screen(self):

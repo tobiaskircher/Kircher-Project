bea1f0e46ff639e75920b10beb47852622b3831d
Tobias K <55653566+tobiaskircher@users.noreply.github.com>
2021-05-06 17:43:33 +0100

    print cube
    prints cube as a 2d net. did this by hard coding what each coordinate maps to on a 2d net. also some corrections to the pieces and colours

--- Project/cube.py
+++ Project/cube.py
@@ -34,7 +34,7 @@ class Cube():
         self.wb = Piece([-1,-1,0],["B","W",None])
         self.wr = Piece([0,-1,-1],[None,"W","R"])
         self.wg = Piece([1,-1,0],["G","W",None])
-        self.wo = Piece([0,-1,-1],[None,"W","O"])
+        self.wo = Piece([0,-1,1],[None,"W","O"])
         self.br = Piece([-1,0,-1],["B",None,"R"])
         self.bo = Piece([-1,0,1],["B",None,"O"])
         self.gr = Piece([1,0,-1],["G",None,"R"])
@@ -44,8 +44,8 @@ class Cube():
         #eg ybr = yellow+blue+red corner piece
         self.ybr = Piece([-1,1,-1],["B","Y","R"])
         self.ybo = Piece([-1,1,1],["B","Y","O"])
-        self.ygr = Piece([1,1,-1],["G","Y","O"])
-        self.ygo = Piece([1,1,1],["G","Y","R"])
+        self.ygo = Piece([1,1,1],["G","Y","O"])
+        self.ygr = Piece([1,1,-1],["G","Y","R"])
         self.wbr = Piece([-1,-1,-1],["B","W","R"])
         self.wbo = Piece([-1,-1,1],["B","W","O"])
         self.wgr = Piece([1,-1,-1],["G","W","R"])
@@ -67,7 +67,73 @@ class Cube():
         return None
 
     def __str__(self):
-        return "Print cube."
+        #empty list for cube
+        cube = ["?"] * 54
+
+        convert_3d_to_2d = [
+            [[0,1,0],[None,4,None]],
+            [[-1,0,0],[13,None,None]],
+            [[0,0,-1],[None, None, 22]],
+            [[1,0,0],[31,None,None]],
+            [[0,0,1],[None,None,40]],
+            [[0,-1,0],[None,49,None]],
+
+            [[-1,1,0],[10,3,None]],
+            [[0,1,-1],[None,7,19]],
+            [[1,1,0],[28,5,None]],
+            [[0,1,1],[None,1,37]],
+            [[-1,-1,0],[16,48,None]],
+            [[0,-1,-1],[None,46,25]],
+            [[1,-1,0],[34,50,None]],
+            [[0,-1,1],[None,52,43]],
+            [[-1,0,-1],[14,None,21]],
+            [[-1,0,1],[12,None,41]],
+            [[1,0,-1],[30,None,23]],
+            [[1,0,1],[32,None,39]],
+
+            [[-1,1,-1],[11,6,18]],
+            [[-1,1,1],[9,0,38]],
+            [[1,1,1],[29,2,36]],
+            [[1,1,-1],[27,8,20]],
+            [[-1,-1,-1],[17,45,24]],
+            [[-1,-1,1],[15,51,44]],
+            [[1,-1,-1],[33,47,26]],
+            [[1,-1,1],[35,53,42]]
+            ]
+        
+        for point in convert_3d_to_2d:
+            piece = self.get_piece(point[0])            
+            for index in range(len(point[1])):
+                if point[1][index] != None:
+                    cube[point[1][index]] = piece.colours[index]
+
+        #create net
+        text = ""
+        
+        #yellow face
+        for i in range(0,9,3):
+            text += (" " * 4) + "".join(cube[i:i+3]) + "\n"
+        text += "\n"
+        
+        #blue, red, green and orange face
+        line1, line2, line3 = "","",""
+        for i in range(1,5):
+            line1 += "".join(cube[i*9:i*9+3])+ " "
+            line2 += "".join(cube[i*9+3:i*9+6]) + " "
+            line3 += "".join(cube[i*9+6:i*9+9]) + " "
+        line1 += "\n"
+        line2 += "\n"
+        line3 += "\n"
+
+        text += line1 + line2 + line3
+
+        #white face
+        text += "\n"
+        for i in range(45,53,3):
+            text += (" " * 4) + "".join(cube[i:i+3]) + "\n"
+        
+        return text
+    
                 
 
 
@@ -75,5 +141,5 @@ class Cube():
 
 test_piece = Piece([1,0,-1],["B","W",None])
 test_cube = Cube()
-#print(test_cube)
-print(test_cube.get_piece([1,1,1]))
+print(test_cube)
+

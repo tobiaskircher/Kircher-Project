2963a8da85d3cbc7162bd62bfcd837172919779f
Tobias K <55653566+tobiaskircher@users.noreply.github.com>
2021-05-06 13:07:08 +0100

    basic cube class
    sets up a sorted cube with regular colours.

---
+++ Project/cube.py
@@ -0,0 +1,61 @@
+#Free Mode
+class Piece():
+    def __init__(self, position, colours):
+        #[0,0,0] being centre piece (does not exist, in middle of cube)
+        self.position = position
+        #[clr facing x dir, clr facing y dir, clr facing z dir]
+        self.colours = colours
+
+    def __str__(self):
+        line1 = "Position: " + str(self.position)
+        line2 = "Colours: " + str(self.colours)
+        return line1 + "\n" + line2
+        
+class Cube():
+    def __init__(self):
+        #initiate cube pieces
+        #orientation: yellow facing up, red facing towards you
+        
+        #centre pieces
+        #eg. y = yellow center piece
+        self.y = Piece([0,1,0],[None,"Y",None])
+        self.b = Piece([-1,0,0],["B",None,None])
+        self.r = Piece([0,0,-1],[None,None,"R"])
+        self.g = Piece([1,0,0],["G",None,None])
+        self.o = Piece([0,0,1],[None,None,"O"])
+        self.w = Piece([0,-1,0],[None,"W",None])
+
+        #edge pieces
+        #eg. yb = yellow + blue edge piece
+        self.yb = Piece([-1,1,0],["B","Y",None])
+        self.yr = Piece([0,1,-1],[None,"Y","R"])
+        self.yg = Piece([1,1,0],["G","Y",None])
+        self.yo = Piece([0,1,1],[None,"Y","O"])
+        self.wb = Piece([-1,-1,0],["B","W",None])
+        self.wr = Piece([0,-1,-1],[None,"W","R"])
+        self.wg = Piece([1,-1,0],["G","W",None])
+        self.wo = Piece([0,-1,-1],[None,"W","O"])
+        
+        self.br = Piece([-1,0,-1],["B",None,"R"])
+        self.bo = Piece([-1,0,1],["B",None,"O"])
+        self.gr = Piece([1,0,-1],["G",None,"R"])
+        self.go = Piece([1,0,1],["G",None,"O"])
+
+        #corner pieces
+        #eg ybr = yellow+blue+red corner piece
+        self.ybr = Piece([-1,1,-1],["B","Y","R"])
+        self.ybo = Piece([-1,1,1],["B","Y","O"])
+        self.ygr = Piece([1,1,-1],["G","Y","O"])
+        self.ygo = Piece([1,1,1],["G","Y","R"])
+
+        self.wbr = Piece([-1,-1,-1],["B","W","R"])
+        self.wbo = Piece([-1,-1,1],["B","W","O"])
+        self.wgr = Piece([1,-1,-1],["G","W","R"])
+        self.wgo = Piece([1,-1,1],["G","W","O"])
+                
+
+
+
+
+test_piece = Piece([1,0,-1],["B","W",None])
+test_cube = Cube()

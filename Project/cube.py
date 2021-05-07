2ee4223a0433973b034beb33718e571abd623e24
Tobias K <55653566+tobiaskircher@users.noreply.github.com>
2021-05-07 15:13:54 +0100

    fixed z axis
    fixed the z axis so now the z axis goes in the correct positive direction

--- Project/cube.py
+++ Project/cube.py
@@ -35,36 +35,36 @@ class Cube():
         #eg. y = yellow center piece
         self.y = Piece([0,1,0],[None,"Y",None])
         self.b = Piece([-1,0,0],["B",None,None])
-        self.r = Piece([0,0,-1],[None,None,"R"])
+        self.r = Piece([0,0,1],[None,None,"R"])
         self.g = Piece([1,0,0],["G",None,None])
-        self.o = Piece([0,0,1],[None,None,"O"])
+        self.o = Piece([0,0,-1],[None,None,"O"])
         self.w = Piece([0,-1,0],[None,"W",None])
 
         #edge pieces
         #eg. yb = yellow + blue edge piece
         self.yb = Piece([-1,1,0],["B","Y",None])
-        self.yr = Piece([0,1,-1],[None,"Y","R"])
+        self.yr = Piece([0,1,1],[None,"Y","R"])
         self.yg = Piece([1,1,0],["G","Y",None])
-        self.yo = Piece([0,1,1],[None,"Y","O"])
+        self.yo = Piece([0,1,-1],[None,"Y","O"])
         self.wb = Piece([-1,-1,0],["B","W",None])
-        self.wr = Piece([0,-1,-1],[None,"W","R"])
+        self.wr = Piece([0,-1,1],[None,"W","R"])
         self.wg = Piece([1,-1,0],["G","W",None])
-        self.wo = Piece([0,-1,1],[None,"W","O"])
-        self.br = Piece([-1,0,-1],["B",None,"R"])
-        self.bo = Piece([-1,0,1],["B",None,"O"])
-        self.gr = Piece([1,0,-1],["G",None,"R"])
-        self.go = Piece([1,0,1],["G",None,"O"])
+        self.wo = Piece([0,-1,-1],[None,"W","O"])
+        self.br = Piece([-1,0,1],["B",None,"R"])
+        self.bo = Piece([-1,0,-1],["B",None,"O"])
+        self.gr = Piece([1,0,1],["G",None,"R"])
+        self.go = Piece([1,0,-1],["G",None,"O"])
 
         #corner pieces
         #eg ybr = yellow+blue+red corner piece
-        self.ybr = Piece([-1,1,-1],["B","Y","R"])
-        self.ybo = Piece([-1,1,1],["B","Y","O"])
-        self.ygo = Piece([1,1,1],["G","Y","O"])
-        self.ygr = Piece([1,1,-1],["G","Y","R"])
-        self.wbr = Piece([-1,-1,-1],["B","W","R"])
-        self.wbo = Piece([-1,-1,1],["B","W","O"])
-        self.wgr = Piece([1,-1,-1],["G","W","R"])
-        self.wgo = Piece([1,-1,1],["G","W","O"])
+        self.ybr = Piece([-1,1,1],["B","Y","R"])
+        self.ybo = Piece([-1,1,-1],["B","Y","O"])
+        self.ygo = Piece([1,1,-1],["G","Y","O"])
+        self.ygr = Piece([1,1,1],["G","Y","R"])
+        self.wbr = Piece([-1,-1,1],["B","W","R"])
+        self.wbo = Piece([-1,-1,-1],["B","W","O"])
+        self.wgr = Piece([1,-1,1],["G","W","R"])
+        self.wgo = Piece([1,-1,-1],["G","W","O"])
 
         self.pieces = [
             self.y, self.b, self.r, self.g, self.o, self.w,
@@ -165,32 +165,32 @@ class Cube():
         convert_3d_to_2d = [
             [[0,1,0],[None,4,None]],
             [[-1,0,0],[13,None,None]],
-            [[0,0,-1],[None, None, 22]],
+            [[0,0,1],[None, None, 22]],
             [[1,0,0],[31,None,None]],
-            [[0,0,1],[None,None,40]],
+            [[0,0,-1],[None,None,40]],
             [[0,-1,0],[None,49,None]],
 
             [[-1,1,0],[10,3,None]],
-            [[0,1,-1],[None,7,19]],
+            [[0,1,1],[None,7,19]],
             [[1,1,0],[28,5,None]],
-            [[0,1,1],[None,1,37]],
+            [[0,1,-1],[None,1,37]],
             [[-1,-1,0],[16,48,None]],
-            [[0,-1,-1],[None,46,25]],
+            [[0,-1,1],[None,46,25]],
             [[1,-1,0],[34,50,None]],
-            [[0,-1,1],[None,52,43]],
-            [[-1,0,-1],[14,None,21]],
-            [[-1,0,1],[12,None,41]],
-            [[1,0,-1],[30,None,23]],
-            [[1,0,1],[32,None,39]],
+            [[0,-1,-1],[None,52,43]],
+            [[-1,0,1],[14,None,21]],
+            [[-1,0,-1],[12,None,41]],
+            [[1,0,1],[30,None,23]],
+            [[1,0,-1],[32,None,39]],
 
-            [[-1,1,-1],[11,6,18]],
-            [[-1,1,1],[9,0,38]],
-            [[1,1,1],[29,2,36]],
-            [[1,1,-1],[27,8,20]],
-            [[-1,-1,-1],[17,45,24]],
-            [[-1,-1,1],[15,51,44]],
-            [[1,-1,-1],[33,47,26]],
-            [[1,-1,1],[35,53,42]]
+            [[-1,1,1],[11,6,18]],
+            [[-1,1,-1],[9,0,38]],
+            [[1,1,-1],[29,2,36]],
+            [[1,1,1],[27,8,20]],
+            [[-1,-1,1],[17,45,24]],
+            [[-1,-1,-1],[15,51,44]],
+            [[1,-1,1],[33,47,26]],
+            [[1,-1,-1],[35,53,42]]
             ]
         
         for point in convert_3d_to_2d:

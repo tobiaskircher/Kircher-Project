d34077914a07f20409021d08f85b57644d6830af
Tobias K <55653566+tobiaskircher@users.noreply.github.com>
2022-01-25 18:44:21 +0000

    another error fixed
    always went straight to unsolvable, this was due to the try loop, error was in rubik_solver, apparently not having utils although this was required on other device

--- Project/solution_generator.py
+++ Project/solution_generator.py
@@ -1,11 +1,11 @@
 #finds solution to rubiks cube scramble
-#pip instal rubik_solver
+#pip install rubik_solver
 
-import rubik_solver
+from rubik_solver import utils
 
 def run(cube):
     
-    solution = rubik_solver.utils.solve(cube, "Kociemba")
+    solution = utils.solve(cube, "Kociemba")
     #print(solution)
 
     return solution
